3a0ad7b935bf789394f96d50f9bd73f069852967
Held Grijo <held.grijo@gmail.com>
2024-10-11 16:42:56 -0400

    add base springboot ws project

---
+++ src/main/java/com/example/wssec/WssecApplication.java
@@ -0,0 +1,13 @@
+package com.example.wssec;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WssecApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WssecApplication.class, args);
+	}
+
+}
